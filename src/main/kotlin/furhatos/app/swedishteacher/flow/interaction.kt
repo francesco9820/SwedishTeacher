e547439d2044b5fb34a39e7d1bfe84d8c6d0e087
annettasillard <annettasillard@hotmail.com>
2021-12-13 13:44:23 +0100

    auto-generated files

--- src/main/kotlin/furhatos/app/swedishteacher/flow/interaction.kt
+++ src/main/kotlin/furhatos/app/swedishteacher/flow/interaction.kt
@@ -3,17 +3,41 @@ package furhatos.app.swedishteacher.flow
 import furhatos.nlu.common.*
 import furhatos.flow.kotlin.*
 
+val Options =  state(Interaction){
+    onResponse {
+        println("Caught response not matching any of my intents nr 2")
+    }
+    onResponse<Goodbye> {
+        furhat.say("Alright, bye")
+        goto(Idle)
+    }
+}
+
+val RequestName: State = state(parent = Options){
+    onEntry {
+        furhat.ask("What may I call you?")
+    }
+    onResponse<PersonName> {
+        furhat.say("You have a lovely name " + it.intent)
+    }
+    onResponse{
+        furhat.say("Sorry, I am pretty bad with names actually. I’ll just call you buddy.")
+    }
+}
+
 val Start : State = state(Interaction) {
 
     onEntry {
-        furhat.ask("Hi there. Do you like robots?")
+        random(
+            {furhat.ask("Hi there")},
+            {furhat.ask("Hello")}
+        )
     }
-
-    onResponse<Yes>{
-        furhat.say("I like humans.")
+    onResponse<Greeting> {
+        goto(RequestName)
     }
-
-    onResponse<No>{
-        furhat.say("That's sad.")
+    onResponse {
+        println("Caught response not matching any of my intents")
+        goto(RequestName)
     }
 }
