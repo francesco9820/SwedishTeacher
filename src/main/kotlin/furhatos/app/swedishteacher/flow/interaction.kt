3fe0355abb939c8365e7fd33666e0f635f12460c
annettasillard <annettasillard@hotmail.com>
2021-12-14 13:31:55 +0100

    - Intent for requesting what types of vocabulary words are available to practice - Entity for the types of vocabulary that (will) exist

--- src/main/kotlin/furhatos/app/swedishteacher/flow/interaction.kt
+++ src/main/kotlin/furhatos/app/swedishteacher/flow/interaction.kt
@@ -1,48 +1,71 @@
 package furhatos.app.swedishteacher.flow
 
 import furhatos.nlu.common.*
+import furhatos.app.swedishteacher.nlu.*
 import furhatos.flow.kotlin.*
+import furhatos.util.Language
 
 /*
 We can use Options state as the parent for other states,
 in order to catch other types of responses
 */
 val Options =  state(Interaction){
-    onResponse {
-        println("Caught response not matching any of my intents nr 2")
-    }
+
     onResponse<Goodbye> {
         furhat.say("Alright, bye")
         goto(Idle)
     }
 
+    onResponse<RequestVocabularyTypes> {
+        furhat.say("I know so many words")
+        furhat.say("We could practice ${VocabularyType().getEnum(Language.ENGLISH_US).joinToString(", ")}")
+        //furhat.ask("Which one would you want to practice?")
+    // If user asks what types of vocabulary are available
+       // Should list all types of vocabulary like colors, clothing etc
+    }
+    onResponse {
+        println("Caught response not matching any of my intents nr 2")
+    }
+
+
+    /*
+    onResponse<ChooseVocabulary>{
+        If users chooses a vocabulary type,
+        the type should be sent to a function that stores it on the user
+    }
+     */
+
 }
+
 
 val IntroVocabulary : State = state(parent = Options){
     onEntry {
         furhat.ask("Alright, are you ready to become a kick-ass Swedish speaker?")
     }
     onResponse<Yes> {
-        //goto()
+        furhat.ask("What type of Swedish vocabulary do you want to practice?")
     }
     onResponse<No> {
         furhat.say("We could also talk about the weather but I never go outside. " +
                 "So let’s stick to the stuff I am good at. ")
-        //goto()
+        furhat.ask("What type of Swedish vocabulary do you want to practice?")
     }
 }
 
 
-//capturing user name
-val RequestName: State = state(parent = Options){
+//capturing user's name
+//name still needs to be stored on the user.
+val RequestName: State = state(Interaction){
     onEntry {
         furhat.ask("What may I call you?")
     }
     onResponse<PersonName> {
         furhat.say("You have a lovely name " + it.intent)
+        goto(IntroVocabulary)
     }
     onResponse{
         furhat.say("Sorry, I am pretty bad with names actually. I’ll just call you buddy.")
+        goto(IntroVocabulary)
     }
 }
 
--- src/main/kotlin/furhatos/app/swedishteacher/nlu/nlu.kt
+++ src/main/kotlin/furhatos/app/swedishteacher/nlu/nlu.kt
@@ -1,1 +1,28 @@
 package furhatos.app.swedishteacher.nlu
+
+
+import furhatos.nlu.EnumEntity
+import furhatos.nlu.Intent
+import furhatos.util.Language
+
+
+//Vocabulary type entity
+class VocabularyType : EnumEntity(stemming = true, speechRecPhrases = true){
+    override fun getEnum(lang: Language): List<String> {
+        return listOf("colors", "clothing items", "numbers")
+    }
+}
+
+class RequestVocabularyTypes: Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("What options do you have?",
+            "Which vocabulary can I practice?",
+            "What are the alternatives?",
+            "What could I practice?",
+            "Which words can you teach me?",
+            "Which words do you know?",
+            "What can you teach me?"
+
+        )
+    }
+}
